a38058887d1d0872f0db6919363b7d69a28f9cce
Victormuthomi <victormuhomi100@gmail.com>
2024-09-29 00:21:55 +0300

    Add corsheaders to the settings.py.

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #3rd party 
     'rest_framework',
+    'corsheaders',
 
     #Local 
     'todos.apps.TodosConfig', #hew
@@ -56,12 +57,20 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+#new 
+CORS_ORIGIN_WHITELIST = (
+        'http://localhost:3000',
+        'htpp://localhost:8000',
+)
+
 
 ROOT_URLCONF = 'todo_project.urls'
 
